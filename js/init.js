896364f36a86118883d9b2b8e066b98142048ec2
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-06-03 12:45:36 -0700

    cards functionality

--- js/init.js
+++ js/init.js
@@ -15,8 +15,91 @@ M.textareaAutoResize($('#textarea1'));
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+document.getElementById("index-first").addEventListener("click", myFunction);
+console.log("yaaas") 
+window.stop = false
+function myFunction() {
+  var go
+   console.log(window.stop) 
+  if (window.stop == false){
+      window.stop = true
+      go = 0
+      console.log(go)
+  }
+  else{
+    window.stop = false
+    go = 0    
+    console.log(go)
+  }
+console.log("yaaas")
+    var tl = new TimelineMax({
+      repeat: -1
+    });
+   var positions
+   positions =  getlocation("mountain")
+   console.log(positions.left)
+      tl.to("#mountain", go, {
+        backgroundPosition: positions.left +" 0px",
+        force3D:true,
+        rotation:0.01,
+        z:0.01,
+        autoRound:false,
+        ease: Linear.easeNone
+      });
+
+    positions =  getlocation("ground")
+    console.log(positions.left)
+    var tl = new TimelineMax({
+      repeat: -1
+    });
+
+      tl.to("#ground", go, {
+        backgroundPosition: positions.left +" 0px",
+        force3D:true,
+        rotation:0.01,
+        z:0.01,
+        autoRound:false,
+        ease: Linear.easeNone
+      });
+    return tl;
+
+   
+
+  function getlocation(ID){
+    var demo = document.getElementById(ID),
+       _tmp = window.getComputedStyle(demo,null).backgroundPosition.trim().split(/\s+/),
+        positions = {
+        'left' : _tmp[0],
+        'top' : _tmp[1]
+    };
+    return positions  
+
+  }
+
+}
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    function cactus() {
+
+    var tl = new TimelineMax({
+      repeat: -1
+    });
+
+    tl.to(".cactus", 50, {
+        backgroundPosition: "1301px 0px",
+        force3D:true,
+        rotation:0.01,
+        z:0.01,
+        autoRound:false,
+        ease: Linear.easeNone
+      });
+
+    return tl;
+  }
 
   function ground() {
 
@@ -50,6 +133,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         autoRound:false,
         ease: Linear.easeNone
       });
+
+   
+   
 
       // document.getElementById("mountain").addEventListener("click", myFunction);
 
@@ -86,6 +172,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 window.onload = function() {
   
   masterTL
+  .add(cactus(),0)
   .add(ground(),0)
   .add(mountain(), 0)
   .timeScale(0.7)
